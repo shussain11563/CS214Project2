cb0114be2897b9ab73c917e6d5dff2cd9ae1a098
shussain11563 <44513446+shussain11563@users.noreply.github.com>
2021-04-14 03:50:38 -0400

    Basic Queue

--- compare.c
+++ compare.c
@@ -11,44 +11,56 @@
 #endif
 
 //directory queue
-/*
+
 typedef struct Queue
 {
-    char** array;
-    int count;
-    int size;
-
+    unsigned size;
+    strbuf_t* array;
+    unsigned count;
+    unsigned head;
 
 } Queue;
-*/
 
-void init(Queue* queue, int count)
+
+void init(Queue* queue)
 {
-    queue->size = count;
+    queue->size = 3;  //test with realloc
     queue->count = 0;
-    queue->array = malloc(sizeof(char*)  * queue->size);
-    for(int i = 0; i < queue->size; i++)
-    {
-        queue->array[i] = malloc(sizeof(char)*10);
-    }
-
+    queue->head = 0;
+    queue->array = malloc(sizeof(strbuf_t) * queue->size);
 
 }
 
-void insert(Queue* queue, char* word)
+
+
+void insert(Queue* queue, char* string)
 {
-    strcpy((queue->array)[queue->count], word);
+    if(queue->count+1==queue->size)
+    {
+        size_t size = queue->size+1;
+        strbuf_t* temp = realloc(queue->array, sizeof(strbuf_t)*size);  
+        queue->array = temp;
+        queue->size = size;
+    }
+    strbuf_t word;
+    sb_init(&word, 5);
+    sb_concat(&word, string);
+    queue->array[queue->count] = word;
     queue->count++;
+}
+
+void dequeue(Queue* queue, char* string)
+{
+
 }
 
 void destroy(Queue* queue)
 {
-    for(int i = 0; i < queue->size; i++)
+    for(int i = 0; i < queue->count; i++)
     {
-        free(queue->array[i]);
+        free(queue->array[i].data);
     }
     free(queue->array);
-
 }
 
 
@@ -58,15 +70,15 @@ void destroy(Queue* queue)
 int main(int argc, char* argv[])
 {
     Queue q;
-    init(&q, argc);
+    init(&q);
 
     for(int i = 0; i < argc; i++)
     {
         insert(&q, argv[i]);
     }
-    for(int i = 0; i < q.size; i++)
+    for(int i = 0; i < q.count; i++)
     {
-        printf("----> %s\n", q.array[i]);
+        printf("%d----> %s\n", q.count, q.array[i].data);
     }
     destroy(&q);
 }
